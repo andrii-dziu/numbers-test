44f00f8a78bf036fa53ee3d64b9547d05865d0d9
AndriiDziuraDev <andrii.dziura.dev@gmail.com>
2024-02-23 23:38:01 +1100

    fix/add ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule } from '@ngrx/store';
+import * as sumReducer from './store/reducers';
 
 @NgModule({
   declarations: [AppComponent, HomePageComponent],
@@ -13,7 +17,11 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    StoreModule.forRoot({}, {}),
+    StoreModule.forFeature('sum', sumReducer.sumReducer),
+    EffectsModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: true }),
   ],
   providers: [],
   bootstrap: [AppComponent],
